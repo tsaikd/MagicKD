130a862217aba002a72b6c65b3d57bac515f32be
tsaikd <tsaikd@c46d52d6-3907-0410-955f-e2ce1611362d>
2006-06-13 22:29:05 +0000

    git-svn-id: http://192.168.20.152/svn/MagicKD@109 c46d52d6-3907-0410-955f-e2ce1611362d

---
+++ tags/1.0.2.0/MagicKDConf.cpp
@@ -0,0 +1,28 @@
+#include "StdAfx.h"
+#include "Language.h"
+#include "MagicKDConf.h"
+
+#define DEFAULT_STARTMIN			false
+#define DEFAULT_SHOWCLOSEWINDOW		true
+#define DEFAULT_CHECKUPDATE			true
+#define DEFAULT_UPDATELASTEST		false
+#define DEFAULT_WALLCHANGER			false
+#define DEFAULT_FINFDUPFILE			false
+#define DEFAULT_TRANSPARENCY		255
+#define DEFAULT_LANGID				FindDefaultLang()
+
+CMagicKDConf::CMagicKDConf()
+{
+}
+
+void CMagicKDConf::Init(CIni *pIni)
+{
+	m_General_bStartMin.Init(pIni, _T("General"), _T("bStartMin"), DEFAULT_STARTMIN);
+	m_General_bShowCloseWindow.Init(pIni, _T("General"), _T("bShowCloseWindow"), DEFAULT_SHOWCLOSEWINDOW);
+	m_General_bCheckUpdate.Init(pIni, _T("General"), _T("bCheckUpdate"), DEFAULT_CHECKUPDATE);
+	m_General_bUpdateLastest.Init(pIni, _T("General"), _T("bUpdateLastest"), DEFAULT_UPDATELASTEST);
+	m_General_uTransparency.Init(pIni, _T("General"), _T("uTransparency"), DEFAULT_TRANSPARENCY);
+	m_General_uLangID.Init(pIni, _T("General"), _T("uLangID"), DEFAULT_LANGID);
+	m_FuncList_bWallChanger.Init(pIni, _T("FuncList"), _T("bWallChanger"), DEFAULT_WALLCHANGER);
+	m_FuncList_bFindDupFile.Init(pIni, _T("FuncList"), _T("bFindDupFile"), DEFAULT_FINFDUPFILE);
+}
