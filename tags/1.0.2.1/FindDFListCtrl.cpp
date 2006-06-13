0c79f03f80c259e90e8c898d6a2459fa7d652e4b
tsaikd <tsaikd@c46d52d6-3907-0410-955f-e2ce1611362d>
2006-06-13 22:26:14 +0000

    git-svn-id: http://192.168.20.152/svn/MagicKD@107 c46d52d6-3907-0410-955f-e2ce1611362d

---
+++ tags/1.0.2.1/FindDFListCtrl.cpp
@@ -0,0 +1,74 @@
+#include "StdAfx.h"
+#include "Resource.h"
+#include "Language.h"
+
+#include "FindDFListCtrl.h"
+
+CFindDFListCtrl::CFindDFListCtrl()
+	:	m_bInit(false)
+{
+}
+
+CFindDFListCtrl::~CFindDFListCtrl()
+{
+}
+
+void CFindDFListCtrl::Init()
+{
+	CRect rcWin;
+	GetClientRect(rcWin);
+	InsertColumn(0, CResString(IDS_FIND_COLUMN_FINDLIST), LVCFMT_LEFT, rcWin.right);
+
+	SetExtendedStyle(GetExtendedStyle() | LVS_EX_FULLROWSELECT);
+
+	m_bInit = true;
+}
+
+void CFindDFListCtrl::Localize()
+{
+	if (!m_bInit)
+		return;
+
+	int iColWidth = GetColumnWidth(0);
+	DeleteColumn(0);
+	InsertColumn(0, CResString(IDS_FIND_COLUMN_FINDLIST), LVCFMT_LEFT, iColWidth);
+}
+
+BEGIN_MESSAGE_MAP(CFindDFListCtrl, CKDListCtrl)
+	ON_WM_CONTEXTMENU()
+	ON_WM_DROPFILES()
+END_MESSAGE_MAP()
+
+void CFindDFListCtrl::OnDropFiles(HDROP hDropInfo)
+{
+	TCHAR sFilePath[MAX_PATH];
+	UINT i, uCount = DragQueryFile(hDropInfo, UINT_MAX, NULL, 0);
+
+	for (i = 0;i< uCount;i++) {
+		DragQueryFile(hDropInfo, i, sFilePath, MAX_PATH);
+
+		if (PathFileExists(sFilePath))
+			InsertItem(GetItemCount(), sFilePath);
+	}
+
+	CKDListCtrl::OnDropFiles(hDropInfo);
+}
+
+void CFindDFListCtrl::OnContextMenu(CWnd* /*pWnd*/, CPoint /*point*/)
+{
+	m_mContextMenu.CreatePopupMenu();
+	POSITION pos = GetFirstSelectedItemPosition();
+
+	m_mContextMenu.AppendMenu(MF_STRING, IDS_FIND_MENU_ADDFINDPATH, CResString(IDS_FIND_MENU_ADDFINDPATH));
+	if (pos) {
+		m_mContextMenu.AppendMenu(MF_STRING, IDS_FIND_MENU_REMOVEFINDPATH, CResString(IDS_FIND_MENU_REMOVEFINDPATH));
+	}
+
+	if (m_mContextMenu.GetMenuItemCount()) {
+		CPoint cpPopMenu;
+		GetCursorPos(&cpPopMenu);
+		m_mContextMenu.TrackPopupMenu(TPM_LEFTALIGN | TPM_RIGHTBUTTON, cpPopMenu.x, cpPopMenu.y, GetParent());
+	}
+	m_mContextMenu.DestroyMenu();
+	Invalidate();
+}
