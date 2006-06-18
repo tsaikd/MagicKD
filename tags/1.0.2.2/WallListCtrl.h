0a9f8b6a86950436c0b1f2ced6b348205c5c0fee
tsaikd <tsaikd@c46d52d6-3907-0410-955f-e2ce1611362d>
2006-06-18 13:28:44 +0000

    git-svn-id: http://192.168.20.152/svn/MagicKD@123 c46d52d6-3907-0410-955f-e2ce1611362d

---
+++ tags/1.0.2.2/WallListCtrl.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "KDListCtrl.h"
+#include "KDIni.h"
+
+class CWallListCtrl :
+	public CKDListCtrl, public CKDIni
+{
+	DECLARE_MESSAGE_MAP()
+public:
+	CWallListCtrl();
+	virtual ~CWallListCtrl();
+
+	void Init(CIni *pIni);
+	virtual void SaveIni();
+
+private:
+	bool m_bInit;
+protected:
+	virtual LRESULT DefWindowProc(UINT message, WPARAM wParam, LPARAM lParam);
+public:
+	afx_msg void OnDestroy();
+};
