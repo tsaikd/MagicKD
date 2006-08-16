18063de7ca3c819af191cc7dd56bd28d2ecb0374
tsaikd <tsaikd@c46d52d6-3907-0410-955f-e2ce1611362d>
2006-08-16 23:23:46 +0000

    git-svn-id: http://192.168.20.152/svn/MagicKD@198 c46d52d6-3907-0410-955f-e2ce1611362d

---
+++ tags/1.0.3.3/KDClass/KDTray.h
@@ -0,0 +1,41 @@
+#pragma once
+
+/*
+
+	if User click on tray icon,
+	then in DefWindowProc()
+		message = UWM_TRAY_CALLBACK,
+		lParam = mouse event (ex: WM_LBUTTONDBLCLK, WM_RBUTTONDOWN)
+
+*/
+
+extern UINT WMU_TRAY_CALLBACK;
+
+class CKDTray
+{
+public:
+	CKDTray();
+	virtual ~CKDTray();
+
+	bool RegisterTray(HWND hWnd, HICON hIcon, LPCTSTR sTip = NULL);
+	void UnRegisterTray();
+
+	LPCTSTR GetTrayTip();
+	bool SetTrayTip(LPCTSTR sTip);
+	bool SetTray(HICON hIcon = NULL, LPCTSTR sTip = NULL);
+
+	CMenu *GetTrayMenu();
+	bool AppendMenu(UINT nFlags, UINT_PTR nIDNewItem = 0, LPCTSTR lpszNewItem = NULL, bool bDefault = false);
+	bool InsertMenu(UINT nPosition, UINT nFlags, UINT_PTR nIDNewItem = 0, LPCTSTR lpszNewItem = NULL, bool bDefault = false);
+	bool ModifyMenu(UINT nID, LPCTSTR lpszNewItem);
+	int FindTrayMenuItem(LPCTSTR sMenuString);
+	bool RemoveTrayMenuItem(LPCTSTR sMenuString);
+	bool RemoveTrayMenuItem(int nMenuID);
+	bool TrackPopupMenu(CWnd *pWnd, UINT nFlags = TPM_LEFTALIGN | TPM_LEFTBUTTON);
+
+	UINT CheckMenuItem(UINT nIDCheckItem, UINT nCheck);
+
+private:
+	NOTIFYICONDATA m_trayData;
+	CMenu m_mTrayMenu;
+};
